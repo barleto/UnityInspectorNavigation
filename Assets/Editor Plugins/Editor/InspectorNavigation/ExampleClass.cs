eedfd63eed34380279c03373ab57cda2c4901310
Ricardo Charf <ricardo.charf@wavy.global>
2019-05-11 20:48:45 -0300

    working selection history - can go back

---
+++ Assets/Editor Plugins/Editor/InspectorNavigation/ExampleClass.cs
@@ -0,0 +1,54 @@
+﻿// Simple example that lets you save the current selection and load it.
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ExampleClass : EditorWindow
+{
+    private int[] selectionIDs;
+
+    [MenuItem("Example/Selection Saver")]
+    private static void Init()
+    {
+        ExampleClass window = (ExampleClass)GetWindow(typeof(ExampleClass));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Save"))
+            SaveSelection();
+
+        if (GUILayout.Button("Load"))
+            LoadLastSavedSelection();
+    }
+
+    void OnSelectionChange()
+    {
+        selectionIDs = Selection.instanceIDs;
+    }
+
+    private void SaveSelection()
+    {
+        var saveStr = "";
+
+        foreach (int i in selectionIDs)
+        {
+            saveStr += i.ToString() + ";";
+        }
+
+        saveStr = saveStr.TrimEnd(char.Parse(";"));
+        EditorPrefs.SetString("SelectedIDs", saveStr);
+    }
+
+    private void LoadLastSavedSelection()
+    {
+        string[] strIDs = EditorPrefs.GetString("SelectedIDs").Split(char.Parse(";"));
+
+        int[] ids = new int[strIDs.Length];
+        for (var i = 0; i < strIDs.Length; i++)
+            ids[i] = int.Parse(strIDs[i]);
+        Selection.instanceIDs = ids;
+    }
+}
--- Assets/Editor Plugins/Editor/InspectorNavigation/InspectorNavigationWindow.cs
+++ Assets/Editor Plugins/Editor/InspectorNavigation/InspectorNavigationWindow.cs
@@ -1,16 +1,130 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class InspectorNavigationWindow : MonoBehaviour {
+public class InspectorNavigationWindow : EditorWindow {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    bool _canPrev = false;
+
+    Stack<HashSet<int>> _history = new Stack<HashSet<int>>();
+
+    [MenuItem("Window/InspectorNavigation")]
+    public static void ShowWindow()
+    {
+        var editor = GetWindow(typeof(InspectorNavigationWindow));
+        editor.minSize = new Vector2(editor.minSize.x, 25);
+        editor.maxSize = new Vector2(editor.maxSize.x, 25);
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        CreateUIElement(() => {
+            if (GUILayout.Button("<-"))
+            {
+                GoPrev();
+            }
+        }, _canPrev);
+        CreateUIElement(() => {
+            if (GUILayout.RepeatButton("<<"))
+            {
+                GoPrev();
+            }
+        }, _canPrev);
+        GUILayout.EndHorizontal();
+    }
+
+    void CreateUIElement(Action element, bool enabled)
+    {
+        if (!enabled)
+        {
+            EditorGUI.BeginDisabledGroup(true);
+        }
+        element();
+        if (!enabled)
+        {
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+
+    void GoPrev()
+    {
+        if (_history.Count < 1)
+        {
+            _canPrev = false;
+        }
+        else if (_history.Count == 1)
+        {
+            _history.Pop();
+            Selection.instanceIDs = new int[0];
+            _canPrev = false;
+            Repaint();
+        }else if (_history.Count > 1) {
+            _history.Pop();
+            var set = _history.Peek();
+            Selection.instanceIDs = ArrayFromSet(set);
+            Repaint();
+        }
+    }
+
+    private void OnSelectionChange()
+    {
+        if (Selection.instanceIDs.Length == 0 && _history.Count == 0) return;
+        if (_history.Count <= 0)
+        {
+            _history.Push(SetFromArray(Selection.instanceIDs));
+            _canPrev = true;
+        }
+        else
+        {
+            var newSelection = SetFromArray(Selection.instanceIDs);
+            if (NewSelectionContainsLastSelection())
+            {
+                _history.Pop();
+            }
+            _canPrev = true;
+            _history.Push(newSelection);
+        }
+        Repaint();
+    }
+
+    private bool NewSelectionContainsLastSelection()
+    {
+        return IsSubsetOf(_history.Peek(), SetFromArray(Selection.instanceIDs));
+    }
+
+    HashSet<int> SetFromArray(int[] array)
+    {
+        var set = new HashSet<int>();
+        foreach (var v in array)
+        {
+            set.Add(v);
+        }
+        return set;
+    }
+
+    int[] ArrayFromSet(HashSet<int> set)
+    {
+        List<int> list = new List<int>();
+        foreach (var v in set)
+        {
+            list.Add(v);
+        }
+        return list.ToArray();
+    }
+
+    bool IsSubsetOf(HashSet<int> subset, HashSet<int> set)
+    {
+        foreach (var v in subset)
+        {
+            if (!set.Contains(v))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
