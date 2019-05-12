5b7d6daac09a4b8da575083091f28f04ab54ac2e
Ricardo Charf <ricardo.charf@wavy.global>
2019-05-11 22:47:28 -0300

    added as toolbar button!

--- Assets/Editor Plugins/Editor/InspectorNavigation/InspectorNavigationWindow.cs
+++ Assets/Editor Plugins/Editor/InspectorNavigation/InspectorNavigationWindow.cs
@@ -3,128 +3,132 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityToolbarExtender;
 
-public class InspectorNavigationWindow : EditorWindow {
-
-    bool _canPrev = false;
-
-    Stack<HashSet<int>> _history = new Stack<HashSet<int>>();
-
-    [MenuItem("Window/InspectorNavigation")]
-    public static void ShowWindow()
+namespace UnityInspectornavigation
+{
+    [InitializeOnLoad]
+    public static class InspectorNavigationToolbarButton
     {
-        var editor = GetWindow(typeof(InspectorNavigationWindow));
-        editor.minSize = new Vector2(editor.minSize.x, 25);
-        editor.maxSize = new Vector2(editor.maxSize.x, 25);
-    }
+        static private bool _canPrev = false;
 
-    void OnGUI()
-    {
-        GUILayout.BeginHorizontal();
-        CreateUIElement(() => {
-            if (GUILayout.Button("<-"))
+        static Stack<HashSet<int>> _history = new Stack<HashSet<int>>();
+
+        static InspectorNavigationToolbarButton()
+        {
+            Selection.selectionChanged -= OnSelectionChanged;
+            Selection.selectionChanged += OnSelectionChanged;
+            ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
+        }
+        
+
+        static void OnToolbarGUI()
+        {
+            CreateInspectorBackButton();
+        }
+
+        static void CreateInspectorBackButton()
+        {
+            var tex = EditorGUIUtility.IconContent(@"d_SpeedScale").image;
+
+            GUI.changed = false;
+
+            CreateUIElement(() => {
+                if (GUI.Button(new Rect(0, 0, 100, 20), new GUIContent("Inspector", tex, "Back in inspector selection."))) {
+                    GoPrev();
+                }
+            }, _canPrev);
+        }
+
+        static void CreateUIElement(Action element, bool enabled)
+        {
+            if (!enabled)
             {
-                GoPrev();
+                EditorGUI.BeginDisabledGroup(true);
             }
-        }, _canPrev);
-        CreateUIElement(() => {
-            if (GUILayout.RepeatButton("<<"))
+            element();
+            if (!enabled)
             {
-                GoPrev();
+                EditorGUI.EndDisabledGroup();
             }
-        }, _canPrev);
-        GUILayout.EndHorizontal();
-    }
+        }
 
-    void CreateUIElement(Action element, bool enabled)
-    {
-        if (!enabled)
+        static void GoPrev()
         {
-            EditorGUI.BeginDisabledGroup(true);
-        }
-        element();
-        if (!enabled)
-        {
-            EditorGUI.EndDisabledGroup();
-        }
-    }
-
-    void GoPrev()
-    {
-        if (_history.Count < 1)
-        {
-            _canPrev = false;
-        }
-        else if (_history.Count == 1)
-        {
-            _history.Pop();
-            Selection.instanceIDs = new int[0];
-            _canPrev = false;
-            Repaint();
-        }else if (_history.Count > 1) {
-            _history.Pop();
-            var set = _history.Peek();
-            Selection.instanceIDs = ArrayFromSet(set);
-            Repaint();
-        }
-    }
-
-    private void OnSelectionChange()
-    {
-        if (Selection.instanceIDs.Length == 0 && _history.Count == 0) return;
-        if (_history.Count <= 0)
-        {
-            _history.Push(SetFromArray(Selection.instanceIDs));
-            _canPrev = true;
-        }
-        else
-        {
-            var newSelection = SetFromArray(Selection.instanceIDs);
-            if (NewSelectionContainsLastSelection())
+            if (_history.Count < 1)
+            {
+                _canPrev = false;
+            }
+            else if (_history.Count == 1)
             {
                 _history.Pop();
+                Selection.instanceIDs = new int[0];
+                _canPrev = false;
             }
-            _canPrev = true;
-            _history.Push(newSelection);
-        }
-        Repaint();
-    }
-
-    private bool NewSelectionContainsLastSelection()
-    {
-        return IsSubsetOf(_history.Peek(), SetFromArray(Selection.instanceIDs));
-    }
-
-    HashSet<int> SetFromArray(int[] array)
-    {
-        var set = new HashSet<int>();
-        foreach (var v in array)
-        {
-            set.Add(v);
-        }
-        return set;
-    }
-
-    int[] ArrayFromSet(HashSet<int> set)
-    {
-        List<int> list = new List<int>();
-        foreach (var v in set)
-        {
-            list.Add(v);
-        }
-        return list.ToArray();
-    }
-
-    bool IsSubsetOf(HashSet<int> subset, HashSet<int> set)
-    {
-        foreach (var v in subset)
-        {
-            if (!set.Contains(v))
+            else if (_history.Count > 1)
             {
-                return false;
+                _history.Pop();
+                var set = _history.Peek();
+                Selection.instanceIDs = ArrayFromSet(set);
             }
         }
-        return true;
-    }
 
+        static private void OnSelectionChanged()
+        {
+            if (Selection.instanceIDs.Length == 0 && _history.Count == 0) return;
+            if (_history.Count <= 0)
+            {
+                _history.Push(SetFromArray(Selection.instanceIDs));
+                _canPrev = true;
+            }
+            else
+            {
+                var newSelection = SetFromArray(Selection.instanceIDs);
+                if (NewSelectionContainsLastSelection())
+                {
+                    _history.Pop();
+                }
+                _canPrev = true;
+                _history.Push(newSelection);
+            }
+        }
+
+        static private bool NewSelectionContainsLastSelection()
+        {
+            return IsSubsetOf(_history.Peek(), SetFromArray(Selection.instanceIDs));
+        }
+
+        static HashSet<int> SetFromArray(int[] array)
+        {
+            var set = new HashSet<int>();
+            foreach (var v in array)
+            {
+                set.Add(v);
+            }
+            return set;
+        }
+
+        static int[] ArrayFromSet(HashSet<int> set)
+        {
+            List<int> list = new List<int>();
+            foreach (var v in set)
+            {
+                list.Add(v);
+            }
+            return list.ToArray();
+        }
+
+        static bool IsSubsetOf(HashSet<int> subset, HashSet<int> set)
+        {
+            foreach (var v in subset)
+            {
+                if (!set.Contains(v))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
 }
